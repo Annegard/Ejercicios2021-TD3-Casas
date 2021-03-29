d0939fa9a29be85ac9240bc6f86cb1663e217641
Alejo Casas <atlcasas.15@gmail.com>
2021-03-29 18:54:03 -0300

    prueba

--- Ej1_SecuenciaLED/src/main.c
+++ Ej1_SecuenciaLED/src/main.c
@@ -3,23 +3,25 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 
-#define BLINK_GPIO GPIO_NUM_32
+#define BOTON_A GPIO_NUM_32
+#define BOTON_B GPIO_NUM_32
 
 void app_main() 
 {
-    gpio_pad_select_gpio(BLINK_GPIO);
-
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+    gpio_pad_select_gpio(BOTON_A);
+    gpio_pad_select_gpio(BOTON_B);
+    
+    gpio_set_direction(BOTON_A, GPIO_MODE_INPUT);
 
     while (1)
     {
         /* code */
         printf("LED ON");
-        gpio_set_level(BLINK_GPIO, 1);
+        gpio_set_level(BOTON_A, 1);
         vTaskDelay(1000/portTICK_PERIOD_MS);
 
         printf("LED OFF");
-        gpio_set_level(BLINK_GPIO, 0);
+        gpio_set_level(BOTON_A, 0);
         vTaskDelay(1000/portTICK_PERIOD_MS);        
     }
     
